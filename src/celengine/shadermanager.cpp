2cb06af2a86cc86197f11f31515ec4633d58f582
Hleb Valoshka <375gnu@gmail.com>
2018-12-04 23:04:49 +0300

    Remove static global std::string

--- src/celengine/shadermanager.cpp
+++ src/celengine/shadermanager.cpp
@@ -51,7 +51,7 @@ static const char* errorFragmentShaderSource =
     "}\n";
 
 
-static const string CommonHeader("#version 120\n");
+static const char* CommonHeader = "#version 120\n";
 
 
 ShaderProperties::ShaderProperties() :
@@ -1531,7 +1531,7 @@ PointSizeCalculation()
 GLVertexShader*
 ShaderManager::buildVertexShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += DeclareLights(props);
     if (props.lightModel == ShaderProperties::SpecularModel)
@@ -1851,7 +1851,7 @@ ShaderManager::buildVertexShader(const ShaderProperties& props)
 GLFragmentShader*
 ShaderManager::buildFragmentShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     // Without GL_ARB_shader_texture_lod enabled one can use texture2DLod
     // in vertext shaders only
@@ -2275,7 +2275,7 @@ ShaderManager::buildFragmentShader(const ShaderProperties& props)
 GLVertexShader*
 ShaderManager::buildRingsVertexShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += DeclareLights(props);
     source += "uniform vec3 eyePosition;\n";
@@ -2329,7 +2329,7 @@ ShaderManager::buildRingsVertexShader(const ShaderProperties& props)
 GLFragmentShader*
 ShaderManager::buildRingsFragmentShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += "uniform vec3 ambientColor;\n";
     source += "vec4 diff = vec4(ambientColor, 1.0);\n";
@@ -2415,7 +2415,7 @@ ShaderManager::buildRingsFragmentShader(const ShaderProperties& props)
 GLVertexShader*
 ShaderManager::buildRingsVertexShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += DeclareLights(props);
 
@@ -2457,7 +2457,7 @@ ShaderManager::buildRingsVertexShader(const ShaderProperties& props)
 GLFragmentShader*
 ShaderManager::buildRingsFragmentShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += "uniform vec3 ambientColor;\n";
     for (unsigned int i = 0; i < props.nLights; i++)
@@ -2567,7 +2567,7 @@ ShaderManager::buildRingsFragmentShader(const ShaderProperties& props)
 GLVertexShader*
 ShaderManager::buildAtmosphereVertexShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += DeclareLights(props);
     source += "uniform vec3 eyePosition;\n";
@@ -2604,7 +2604,7 @@ ShaderManager::buildAtmosphereVertexShader(const ShaderProperties& props)
 GLFragmentShader*
 ShaderManager::buildAtmosphereFragmentShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += "varying vec3 scatterEx;\n";
     source += "varying vec3 eyeDir_obj;\n";
@@ -2657,7 +2657,7 @@ ShaderManager::buildAtmosphereFragmentShader(const ShaderProperties& props)
 GLVertexShader*
 ShaderManager::buildEmissiveVertexShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     source += "uniform float opacity;\n";
 
@@ -2719,7 +2719,7 @@ ShaderManager::buildEmissiveVertexShader(const ShaderProperties& props)
 GLFragmentShader*
 ShaderManager::buildEmissiveFragmentShader(const ShaderProperties& props)
 {
-    string source = CommonHeader;
+    string source(CommonHeader);
 
     if (props.texUsage & ShaderProperties::DiffuseTexture)
     {
